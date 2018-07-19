b8c8d4328f04024b3d8d7658ab885a8c75c9d189
Bharat Bhaskar <bharat.bhaskar@cdk.com>
2018-07-19 16:53:32 +0530

    ReduxReactTestProject
    First Redux React Project

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function(){
+    return [
+        {'title':'Javascript:The dynamic Content'},
+        {'title':'Ruby for Developers'},
+        {'title':'Harry Potter'},
+        {'title':'Half Girlfriend'},
+        {'title':'Full Masti'},
+        {'title':'Grand Chusti'}
+    ];
+}
